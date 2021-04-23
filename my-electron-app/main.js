a4632686f2f0042e501c622debf8883680c7f705
Nabin Kim <knabinn97@gmail.com>
2021-04-23 16:48:28 +0900

    add tray icon, user task

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron')
 const path = require('path')
+
+let tray = null;
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -21,6 +23,13 @@ app.whenReady().then(() => {
             createWindow()
         }
     })
+    let iconPath = path.join(__dirname, '/images/test.png');
+    tray = new Tray(iconPath);
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Test1', type: 'radio' }
+    ])
+    tray.setToolTip('This is my application.');
+    tray.setContextMenu(contextMenu);
 })
 
 app.on('window-all-closed', () => {
@@ -28,3 +37,14 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+app.setUserTasks([
+    {
+        program: process.execPath,
+        arguments: '--new-window',
+        iconPath: process.execPath,
+        iconIndex: 0,
+        title: 'New Window',
+        description: 'Create a new window'
+    }
+])
